fde3dfdc04b752c4871d0274544138e2b4ce1912
Georgios Vlassis <gvlassis@mailbox.org>
2026-04-02 01:16:42 +0200

    Tons of refactoring+added distillation

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from transformers import AutoModelForCausalLM
-from utils import DEFAULT_EVAL_TASKS, load_tokenizer, evaluate, quantize
+from utils import DEFAULT_EVAL_TASKS, load_tokenizer, evaluate, quantize, distil
+
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("--model", help="Model to apply QAD on. Used both as the teacher and the student.", default="Qwen/Qwen2.5-0.5B")
+parser.add_argument("--context_size", help="Overrides model sequence length (calibration+distillation)", type=int, default=1024)
 
 parser.add_argument("--eval_tasks", help="Tasks to evaluate unquantized teacher and quantized student after PTQ and after QAD", nargs="+", default=DEFAULT_EVAL_TASKS)
 parser.add_argument("--eval_limit", help="Total samples used for every evaluation task", type=int, default=100)
 
-parser.add_argument("--ptq_dataset", help="Dataset used for NVFP4 calibration", default="gvlassis/ClimbMix10M")
-parser.add_argument("--ptq_keep", help="Keep calibration dataset in RAM", action="store_true")
-parser.add_argument("--ptq_samples", help="Total documents used for calibration", type=int, default=128)
-parser.add_argument("--ptq_batch_size", help="Documents in every calibration batch", type=int, default=32)
+parser.add_argument("--ptq_dataset", help="Dataset used for NVFP4 calibration (forward only)", default="JeanKaddour/minipile")
+parser.add_argument("--ptq_batches", help="Number of batches used for calibration. We do sequence packing.", type=int, default=128)
+parser.add_argument("--ptq_batch_size", help="Size of batches used for calibration", type=int, default=32)
+
+parser.add_argument("--qad_dataset", help="Dataset used for QAD (forward+backward)", default="JeanKaddour/minipile")
+parser.add_argument("--qad_batches", help="Number of batches used for distillation", type=int, default=128)
+parser.add_argument("--qad_batch_size", help="Size of batches used for distillation", type=int, default=32)
+parser.add_argument("--temperature", help="Temperature used for distillation", type=float, default=1)
 
 parser.add_argument("--device_index", help="GPU index (NVIDIA support only)", type=int, default=0)
 
@@ -32,10 +38,10 @@ evaluate(tokenizer, teacher, args.eval_tasks, args.eval_limit)
 
 student = AutoModelForCausalLM.from_pretrained(args.model)
 student = student.to(device)
-student = quantize(tokenizer, student, args.ptq_dataset, args.ptq_keep, args.ptq_samples, args.ptq_batch_size)
+student = quantize(tokenizer, student, args.ptq_dataset, args.context_size, args.ptq_batches, args.ptq_batch_size)
 
 evaluate(tokenizer, student, args.eval_tasks, args.eval_limit)
 
-# distil(teacher, student)
-#
-# evaluate(tokenizer, student, args.eval_tasks, args.eval_limit)
+distil(tokenizer, teacher, student, args.qad_dataset, args.context_size, args.qad_batches, args.qad_batch_size, args.temperature)
+
+evaluate(tokenizer, student, args.eval_tasks, args.eval_limit)
--- utils.py
+++ utils.py
@@ -1,29 +1,43 @@
+import os
+from itertools import chain
+
 from transformers import AutoTokenizer
-import torch
+from datasets import load_dataset
+
+from torch import inference_mode, tensor, int64, AdamW
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+
 from lm_eval.models.huggingface import HFLM
 from lm_eval import simple_evaluate
-from datasets import load_dataset
+
 import modelopt.torch.quantization as mtq
 
+
 DEFAULT_EVAL_TASKS = ["arc_easy", "hellaswag", "piqa"]
+MAP_BATCH_SIZE = 10_000 # default: 1000
+MAP_NUM_PROC = os.cpu_count()//2
+
 
 def load_tokenizer(model):
     tokenizer = AutoTokenizer.from_pretrained(model)
     
-    # Ensures batched generation works correctly
-    tokenizer.padding_side = "left"
-    
     if tokenizer.pad_token_id is None:
         tokenizer.pad_token = tokenizer.eos_token
 
+    # Ensures batched generation works correctly
+    tokenizer.padding_side = "left"
+
     return tokenizer
+
 
 def get_model_device(model):
     device = next(model.parameters()).device
 
     return device
 
-@torch.inference_mode()
+
+@inference_mode()
 def evaluate(tokenizer, model, tasks=DEFAULT_EVAL_TASKS, limit=100):
     hflm = HFLM(tokenizer=tokenizer, pretrained=model)
 
@@ -49,26 +63,110 @@ def evaluate(tokenizer, model, tasks=DEFAULT_EVAL_TASKS, limit=100):
 
     print(results)
 
-def get_ptq_dataset(dataset, keep_in_memory=False, samples=128):
-    ptq_dataset = load_dataset(dataset, split="train", keep_in_memory=keep_in_memory)
-    ptq_dataset = ptq_dataset[:samples]["text"]
 
-    return ptq_dataset
+def get_dataloader(tokenizer, dataset, context_size=1024, batch_size=32):
+    """
+        Practical and clean sequence packing. No truncation or padding. Non-overlapping sequences are loaded sequentially and deterministically. For convenience, a tiny number of tail tokens are dropped.
 
-def quantize(tokenizer, model, dataset, keep_in_memory=False, samples=128, batch_size=32):
-    dataset = get_ptq_dataset(dataset, keep_in_memory, samples)
-    batches = samples//batch_size
-    device = get_model_device(model)
+        However, caching does not work in practise (opt for manual Dataset.save_to_disk()), and multiple context sizes are not supported (rather save as 1D).
+    """
     
-    @torch.inference_mode()
-    def forward_loop(model):
-        for batch_idx in range(batches):
-            batch = dataset[batch_idx*batch_size:(batch_idx+1)*batch_size]
+    dataset = load_dataset(dataset, split="train")
+    
+    def tokenize(batch):
+        """batch = {"text": []}"""
+        
+        batch = tokenizer(
+            batch["text"],
+            add_special_tokens=False, # DANGER
+            padding=False, # Dynamic padding more efficient (not used here)
+            truncation=False,
+            stride=0,
+            return_overflowing_tokens=False,
+            verbose=False
+        )
 
-            batch = tokenizer(batch, return_tensors="pt", padding=True, truncation=True)
-            
-            model(batch["input_ids"].to(device), batch["attention_mask"].to(device))
+        batch = batch["input_ids"]
 
-    model = mtq.quantize(model, mtq.NVFP4_DEFAULT_CFG, forward_loop)
+        batch = [sample+[tokenizer.eos_token_id] for sample in batch]
 
-    return model
+        return {"input_ids": batch}
+
+    def pack(batch):
+        """batch = {"input_ids": []}"""
+
+        batch = batch["input_ids"]
+        batch = chain.from_iterable(batch)
+        batch = list(batch)
+        
+        # Drop tail (otherwise I cannot use map()). Negligible impact for large batch size.
+        samples = len(batch) // context_size
+
+        batch = [batch[i*context_size : (i+1)*context_size] for i in range(samples)]
+
+        return {"input_ids": batch}
+
+    def tokenize_and_pack(batch):
+        """batch = {"input_ids": []}"""
+
+        batch = tokenize(batch)
+        batch = pack(batch)
+
+        return batch
+    
+    # map() enables caching, memory mapping, multiprocessing and returns Dataset
+    dataset = dataset.map(
+        tokenize_and_pack,
+        batched=True,
+        batch_size=MAP_BATCH_SIZE,
+        remove_columns=dataset.column_names,
+        num_proc=MAP_NUM_PROC
+    )
+
+    def collate_fn(batch):
+        """batch = [{"input_ids": []}]"""
+        
+        batch = [sample["input_ids"] for sample in batch]
+        batch = tensor(batch, dtype=int64)
+
+        return {"input_ids": batch, "labels": batch.clone()} # Models shift labels internally
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size,
+        shuffle=False, # Reshuffling at the START of every epoch
+        sampler=None,
+        num_workers=0, # For LLMs, keep at 0 (loading from main thread/synchronous)
+        collate_fn=collate_fn,
+        pin_memory=False, # Page-locked RAM
+        drop_last=True,
+        persistent_workers=False
+    )
+    
+    return dataloader
+
+
+def quantize(tokenizer, model, dataset, context_size=1024, batches=128, batch_size=32):
+    dataloader = get_dataloader(tokenizer, dataset, context_size, batch_size)
+    device = get_model_device(teacher)
+
+    optimizer = AdamW(student.parameters(), lr=1e-5)
+
+    for batch_idx, batch in zip(range(batches), dataloader):
+
+        with inference_mode():
+            teacher_logits = teacher(batch["input_ids"].to(device)).logits # (batch_size, context_size, vocab_size)
+            teacher_logits = teacher_logits[:, :-1, :] # Drop last token (unaligned)
+            teacher_logits = teacher_logits.flatten(0, -2) # (batch_size*context_size, vocab_size)
+
+        student_logits = student(batch["input_ids"].to(device)).logits
+        student_logits = student_logits[:, :-1, :]
+        student_logits = student_logits.flatten(0, -2)
+        
+        # BE CAREFUL OF THE ORDERING
+        loss = kl_div(student_logits, teacher_logits, temperature)
+
+        optimizer.zero_grad()
+        loss.backward()
+        
+        optimizer.step()
